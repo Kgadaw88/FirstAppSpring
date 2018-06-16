c3f52a98b712d10a4294856f26badb494ce8cf75
Kgadaws88 <privatemessage88@gmail.com>
2018-06-16 16:12:46 +0200

    add simply program

---
+++ src/main/java/com/kamilgadawski/firstApp/controllers/CreditController.java
@@ -0,0 +1,31 @@
+package com.kamilgadawski.firstApp.controllers;
+
+
+import com.kamilgadawski.firstApp.models.CreditForm;
+import com.kamilgadawski.firstApp.models.RegisterForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CreditController {
+
+    @GetMapping("/kredyt")
+    public String kredyt(Model model){
+        model.addAttribute("kredyt", new CreditForm());
+        return "kredyt";
+    }
+
+    @PostMapping("/kredyt")
+    @ResponseBody
+    public String credit (@ModelAttribute CreditForm creditForm) {
+
+        if (creditForm.getZarobki() - creditForm.getWydatki() > (creditForm.getWnioskowanaKwota() / creditForm.getMiesiace()) * 0.3 ){
+            return "Dostałem kredyt";
+        }
+        return "Jesteś za bidny";
+    }
+}
---
+++ src/main/java/com/kamilgadawski/firstApp/models/CreditForm.java
@@ -0,0 +1,12 @@
+package com.kamilgadawski.firstApp.models;
+
+
+import lombok.Data;
+
+@Data
+public class CreditForm {
+    private int zarobki;
+    private int wydatki;
+    private int miesiace;
+    private int wnioskowanaKwota;
+}
